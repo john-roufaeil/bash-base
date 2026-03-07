2dcfe7f4f20444bfeda8be31e78a15f9667799b9
ZeyadHMostafa <ZeyadHMostafa@gmail.com>
2026-03-07 10:43:34 +0200

    added main_menu and its functions

---
+++ database-mgmt/connect.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Note we are using returns here because this is intended to be used with source
+
+dbname="$1"
+
+if [[ -z "$dbname" ]]; then
+	error "Usage: connect <database_name>"
+  return 1
+fi
+
+# substitute space for underscore
+dbname="${dbname// /_}"
+
+if ! validate_identifier "$dbname"; then
+  error "Database name must be a valid identifier"
+  return 1
+fi
+
+if [[ ! -d "data/$dbname" ]]; then
+	error "Database '$dbname' not found."
+	return 1
+fi
+
+export CURRENT_DB="$dbname"
+success "Connected to $dbname"
+source menus/db_menu.sh
--- menus/main_menu.sh
+++ menus/main_menu.sh
@@ -1,1 +1,36 @@
 #!/bin/bash
+
+# TODO: I didn't add the ability to select with words yet, not sure aobut it
+
+show_main_menu() {
+	PS3="DBMS-Main> "
+	options=("Create Database" "Drop Database" "List Databases" "Connect to Database" "Exit")
+
+	select opt in "${options[@]}"; do
+		case $opt in
+			"Create Database")
+				read -p -r "Enter Database Name: " dbname
+				./database-mgmt/create.sh "$dbname"
+				;;
+			"Drop Database")
+				read -p -r "Enter Database Name to DROP: " dbname
+				./database-mgmt/drop.sh "$dbname"
+				;;
+			"List Databases")
+				./database-mgmt/list.sh
+				;;
+			"Connect to Database")
+				read -p  -r "Enter Database Name: " dbname
+				# source is important to pass context
+				source ./database-mgmt/connect.sh "$dbname"
+				;;
+			"Exit")
+				info "Goodbye!"
+				exit 0
+				;;
+			*) 
+				error "Invalid option $REPLY"
+				;;
+		esac
+	done
+}
